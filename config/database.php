77189f8ed152606b6667f0e4f0034fa226b9f564
YanaKramareva <iana2000@mail.ru>
2022-05-02 08:56:27 +0300

    Changed database.php

--- config/database.php
+++ config/database.php
@@ -77,7 +77,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => '',
         ],
         /*'default' => env('DB_CONNECTION', 'pgsql'),
         'connections' => [
